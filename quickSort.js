fedeea668d3a7545f050954616542264526f937a
Lakshmi <lakshmi.nishthala@gmail.com>
2018-01-23 13:58:11 -0800

    Trying out quick sort

--- quickSort.js
+++ quickSort.js
@@ -19,4 +19,33 @@ function partition(arr) {
   return arr;
 }
 //0 , 1 ,2 ,3 ,4 ,5 ,6 , 7, 8
-console.log(partition([3, 7, 8, 5, 2, 1, 9, 5, 4]));
+//console.log(partition([3, 7, 8, 5, 2, 1, 9, 5, 4]));
+
+//|6 5 1 3 8 4 7 9 2
+//1 | 5 6 3 8 4 7 9 2
+//1  2 | 6 3 8 4 7 9 5
+
+//Other approach
+//pivot = arr[-1]
+//current Idx -> 0
+//wall = 0
+//if(currentIdx < pivot)
+//swap currentIdx elem with wall
+//wall++
+
+function partitioning(arr, low, high) {
+  let pivot = arr[high];
+  let wall = low;
+  for (let i = low; i < high - 1; i++) {
+    if (arr[i] <= pivot) {
+      //swap current elem with wall
+      [arr[wall], arr[i]] = [arr[i], arr[wall]];
+      wall++;
+    }
+  }
+  //swap wall and pivot
+  [pivot, arr[wall]] = [arr[wall], pivot];
+  return;
+}
+//
+// console.log(quickSort([6,5,1,3,8,4,7,9,2]));
